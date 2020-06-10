1dce1787eeebe3e7382f78cb40d0846fa2a3e296
Daniel Ruiz <druizca@mercadona.es>
2020-06-11 01:13:13 +0200

    Refactor test code
    Extract all target jsons into a module constant

--- test/json_matcher_test.exs
+++ test/json_matcher_test.exs
@@ -1,88 +1,60 @@
 defmodule JSONMatcherTest do
   use ExUnit.Case
 
+  @target_json %{
+    name: "John",
+    surname: "Doe",
+    age: 30,
+    is_smoker: false
+  }
+
   describe "JSONMatcher" do
     test "matches JSON when key and value are found" do
-      json = %{
-        name: "John",
-        surname: "Doe",
-        age: 30
-      }
-
       pattern = %{age: 30}
 
-      assert JSONMatcher.match(pattern, json) == true
+      assert JSONMatcher.match(pattern, @target_json) == true
     end
 
     test "matches JSON when all key-value pairs in pattern are in the target" do
-      json = %{
-        name: "John",
-        surname: "Doe",
-        age: 30,
-        is_smoker: false
-      }
-
       pattern = %{
         name: "John",
         is_smoker: false,
         age: 30
       }
 
-      assert JSONMatcher.match(pattern, json) == true
+      assert JSONMatcher.match(pattern, @target_json) == true
     end
 
     test "does not match JSON when key is not found" do
-      json = %{
-        name: "John",
-        surname: "Doe",
-        age: 30,
-      }
+      pattern = %{gender: "male"}
 
-      assert JSONMatcher.match(%{gender: "male"}, json) == false
+      assert JSONMatcher.match(pattern, @target_json) == false
     end
 
     test "does not match JSON when several keys are not found" do
-      json = %{
-        name: "John",
-        surname: "Doe",
-        age: 30,
-        is_smoker: false
-      }
-
       pattern = %{
         name: "John",
         this_key: "is not found",
         exists_in_json: false
       }
 
-      assert JSONMatcher.match(pattern, json) == false
+      assert JSONMatcher.match(pattern, @target_json) == false
     end
 
     test "does not match JSON when pattern key is found but not its value" do
-      json = %{
-        name: "John",
-        surname: "Doe",
-        age: 30,
-      }
+      pattern = %{surname: "Johnson"}
 
-      assert JSONMatcher.match(%{surname: "Johnson"}, json) == false
+      assert JSONMatcher.match(pattern, @target_json) == false
     end
 
     test "does not match JSON when pattern has several different values" do
-      json = %{
-        name: "John",
-        surname: "Doe",
-        age: 30,
-        is_smoker: false
-      }
-
       pattern = %{
         name: "Jane",
         surname: "Doe",
         is_smoker: true
       }
 
-      assert JSONMatcher.match(pattern, json) == false
+      assert JSONMatcher.match(pattern, @target_json) == false
     end
   end
 end
