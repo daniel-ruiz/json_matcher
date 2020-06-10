bf40844e99991ef7f104c6ed3cdc8aea00efac57
Daniel Ruiz <druizca@mercadona.es>
2020-06-11 01:21:39 +0200

    Refactor code
    - Rename json argument into target
    - Extract some logic into a private function

--- lib/json_matcher.ex
+++ lib/json_matcher.ex
@@ -1,7 +1,11 @@
 defmodule JSONMatcher do
-  def match(pattern, json) do
+  def match(pattern, target) do
     pattern
-    |> Enum.map(fn {key, value} -> Map.has_key?(json, key) and json[key] == value end)
+    |> Enum.map(fn pair -> is_pair_included?(target, pair) end)
     |> Enum.all?
+  end
+
+  defp is_pair_included?(target, {key, value}) do
+    Map.has_key?(target, key) and target[key] == value
   end
 end
